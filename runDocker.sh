840277c3eae39ffbe436c795f395f350f12766d6
Dan <danielmackie82@gmail.com>
2022-07-15 23:24:09 +0800

    removed exposure of port 5432 since we done need to expose the production database outside of the network

--- runDocker.sh
+++ runDocker.sh
@@ -3,7 +3,6 @@
 docker run -d --name my-postgresdb-container \
 --user 1000 \
 -e POSTGRES_PASSWORD=password \
--p 5432:5432 \
 -v $(pwd)/data:/var/lib/postgresql/data \
 --network my-net \
 my-postgres-db \
