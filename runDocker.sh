c05b0a7a1aa52d66dfc1a06cafb04172242ac166
Dan <danielmackie82@gmail.com>
2022-07-20 03:35:34 +0800

    added mochidb.backup to the repo as it's now small emough

--- runDocker.sh
+++ runDocker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker run -d --name my-postgresdb-container \
---user 1000 \
 -e POSTGRES_PASSWORD=password \
+-p 5432:5432 \
 -v $(pwd)/data:/var/lib/postgresql/data \
 --network my-net \
 my-postgres-db \
