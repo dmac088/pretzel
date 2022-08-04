85315acfb95982984629883296f6d0eb73ae6aea
Daniel Mackie <danielmackie@daniels-mbp.lan>
2022-08-04 17:48:52 +0800

    asdf

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export PGPASSWORD=password
-pg_dumpall -h localhost -p 5433 --clean -U mochidb_owner --database=mochidb > mochidb.backup
+pg_dumpall -h localhost -p 5432 --clean -U mochidb_owner --database=mochidb > mochidb.backup
 tar -cvf  archive/"mochidb.backup-$(date +"%Y%m%dT%H%M").gz" mochidb.backup 
